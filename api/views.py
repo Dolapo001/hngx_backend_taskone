8dd486c0090c9a468ba0446af90902f814d7822a
Dolapo24 <adedolapovictoria927@gmail.com>
2023-09-09 21:26:55 +0100

    trackone

--- api/views.py
+++ api/views.py
@@ -1,41 +1,32 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from django.http import JsonResponse
+
 import datetime
-import pytz
-
-from .serializers import InfoSerializer
 
 
-class InfoView(APIView):
-    @staticmethod
-    def get(request):
+# Create your views here.
 
-        slack_name = request.GET.get('slack_name')
+def get_endpoint_data(request):
+    slack_name = request.GET.get('slack_name')
 
-        track = request.GET.get('track')
+    track = request.GET.get('track')
 
-        current_day = datetime.datetime.now(pytz.utc).strftime('%A')
+    current_day = datetime.datetime.utcnow().strftime('%A')
 
-        current_time = datetime.datetime.now(pytz.utc)
+    utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 
-        track = "Backend"
+    github_file_url = "https://github.com/LoneStarrD/hngx_backend_taskone/blob/main/manage.py"
+    github_repo_url = "https://github.com/LoneStarrD/hngx_backend_taskone.git"
 
-        status_code = status.HTTP_200_OK
+    response_data = {
 
-        file_url = "https://github.com/LoneStarrD/hngx_backend_taskone/blob/main/manage.py"
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
 
-        source_code_url = "https://github.com/LoneStarrD/hngx_backend_taskone.git"
+    }
 
-        data = {
-            "slack_name": slack_name,
-            "current_day": current_day,
-            "utc_time": current_time,
-            "track": track,
-            "github_file_url": file_url,
-            "github_repo_url": source_code_url,
-            "statuscode": status_code,
-        }
-
-        serializer = InfoSerializer(data)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    return JsonResponse(response_data)

812eb1bb710b94182af2c30a29aeb99c6ec12c91
Dolapo24 <adedolapovictoria927@gmail.com>
2023-09-09 19:23:49 +0100

    trackone

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import InfoView
 
 urlpatterns = [
-    path('get_info/', InfoView.as_view(), name='get_info'),
+    path('slack_name=adedolapo27&track=backend/', InfoView.as_view(), name='get_info'),
 ]
